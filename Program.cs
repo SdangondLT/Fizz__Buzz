162c2598e0d145d1d5994df6571cded46d8e4160
SdangondLT <sdangond@lean-tech.io>
2022-04-07 19:32:09 -0500

    added fizzBuzz code

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace testOfFizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IsMultipleOfThreeAndFive();
+        }
+
+        public static void IsMultipleOfThreeAndFive()
+        {
+            int numberMain = 100;
+            Console.WriteLine("Estos son los numeros: ");
+
+            for (int i = 1; i <= numberMain; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("fizzbuzz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("buzz");
+                else if (i % 3 == 0)
+                    Console.WriteLine("fizz");
+                else
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
